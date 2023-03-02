5481e7569356b3180dc9200dff4000452605e7f8
Gabriel Dias <gabrieldiascorrea7@gmail.com>
2023-03-01 21:16:14 -0300

    first commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,85 @@
+import { Play } from 'phosphor-react'
+
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartcountdownButton,
+  TaskInput,
+} from './styles'
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod.number()
+    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos.')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
+})
+
+export function Home() {
+  const { register, handleSubmit, watch, formState } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+  })
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data)
+  }
+
+  console.log(formState.errors)
+
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
+  return (
+    <HomeContainer>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
+        <FormContainer>
+          <label htmlFor="">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            placeholder="Dê um nome para seu projeto"
+            {...register('task')}
+          />
+
+          <datalist id="task-suggestions">
+            <option value="projeto-01" />
+            <option value="projeto-02" />
+            <option value="projeto-03" />
+            <option value="Banana" />
+          </datalist>
+
+          <label>durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartcountdownButton type="submit">
+          <Play /> Começar
+        </StartcountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}
